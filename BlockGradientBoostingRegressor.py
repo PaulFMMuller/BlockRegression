66eae03b789c360e32fe822d3aac67a50787981e
PaulFMMuller <paul.fm.muller@gmail.com>
2018-10-06 13:16:41 -0500

    Added Block Linear Regression and Block Gradient Boosting

---
+++ BlockGradientBoostingRegressor.py
@@ -0,0 +1,135 @@
+'''
+Written and developed by Paul Muller.
+'''
+
+from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+
+
+class BlockGradientBoostingRegressor:
+    def __init__(self, n_trees=10, learning_rate=0.1, warm_start=False, **kwargs):
+        self.trees = []
+        self.n_trees = n_trees
+        self.treeArgs = kwargs
+        self.warm_start = warm_start
+        if learning_rate is None:
+            self.gammas = []
+            self.search_gamma = True
+        else:
+            self.search_gamma = False
+            self.learning_rate = learning_rate
+
+
+    def predict(self, X):
+        result = np.zeros(X.shape[0])
+        for i in range(len(self.trees)):
+            if self.search_gamma:
+                gamma_i = self.gammas[i]
+            else:
+                gamma_i = self.learning_rate
+            result += gamma_i * self.trees[i].predict(X)
+        return result
+
+
+    def getTreeTargetsPerBlock(self, X, y):
+        yPred = self.predict(X).reshape(-1)
+        lossGradient = (y - np.sum(yPred)) / X.shape[0]
+        target = np.repeat(lossGradient, X.shape[0])
+        return X,target
+
+
+    def getTreeTargets(self, Xs, ys):
+        X = np.array([])
+        y = np.array([])
+        for i in range(len(Xs)):
+            Xc = Xs[i]
+            yc = ys[i]
+            Xn,yn = self.getTreeTargetsPerBlock(Xc, yc)
+            X = np.stack([X,Xn],axis=0)
+            y = np.stack([y,yn],axis=0)
+        return X,y
+
+
+    '''
+    Xs is a list of numpy arrays, yc is a list of target sum values.
+    The sum of each "real" target variable of the elements of Xs[i] is equal to ys[i].
+    '''
+    def fit(self, Xs, ys):
+        if not self.warm_start:
+            self.trees = []
+            self.gammas = []
+
+        for i in range(self.n_trees):
+            X,y = self.getTreeTargets(Xs, ys)
+            newTree = DecisionTreeRegressor(self.treeArgs)
+            newTree.fit(X, y)
+            self.trees.append(newTree)
+            if self.search_gamma:
+                self.gamma_search(X, y)
+
+        return self
+
+
+    def partial_predict(self, X):
+        result_old = np.zeros(X.shape[0])
+        for i in range(len(self.trees)-1):
+            if self.search_gamma:
+                gamma_i = self.gammas[i]
+            else:
+                gamma_i = self.learning_rate
+            result_old += gamma_i * self.trees[i].predict(X)
+        result_new = self.trees[-1].predict(X)
+        return result_old, result_new
+
+
+    def getGammaComponents(self, Xs):
+        M = np.zeros(len(Xs))
+        N = np.zeros(len(Xs))
+        for i in range(len(Xs)):
+            Xc = Xs[i]
+            result_old,result_new = self.partial_predict(Xc)
+            M[i] = np.sum(result_new.reshape(-1))
+            N[i] = np.sum(result_old.reshape(-1))
+        return M,N
+
+
+    def gamma_search(self, Xs, ys):
+        M,N = self.getGammaComponents(Xs)
+        delta = np.sum(np.array(ys) - N*M)
+        Msq = np.sum(M**2)
+        if Msq < 1e-8:                      # In case the problem is undetermined, keep standard value
+            self.gammas.append(1)
+        else:
+            self.gammas.append(delta / Msq)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BlockLinearRegressor.py
@@ -0,0 +1,71 @@
+'''
+Written and developed by Paul Muller.
+'''
+
+
+
+import numpy as np
+
+
+class BlockLinearRegressor:
+    def __init__(self, fit_intercept=True, lambda_ridge=None):
+        self.beta = []
+        if lambda_ridge is None:
+            self.ridge = False
+        else:
+            self.ridge = True
+            self.lambda_ridge = lambda_ridge
+        self.fit_intercept = fit_intercept
+
+    '''
+    Xs is a list of numpy arrays, yc is a list of target sum values.
+    The sum of each "real" target variable of the elements of Xs[i] is equal to ys[i].
+    '''
+    def fit(self, Xs, ys):
+        M = np.zeros((Xs[0].shape[1], Xs[0].shape[1]))
+        N = np.zeros(Xs[0].shape[1])
+        for i in range(len(Xs)):
+            Xc = Xs[i]
+            yc = ys[i]
+
+            if self.fit_intercept:
+                Xc = np.concatenate([Xc, np.ones((Xc.shape[0], 1))], axis=1)    # Adding intercept column
+            ones = np.ones((Xc.shape[0], 1))
+            M += np.linalg.multi_dot([np.transpose(Xc), ones, np.transpose(ones), Xc])
+            N += yc * np.transpose(Xc).dot(ones)
+
+        if self.ridge:
+            M += self.lambda_ridge * np.eyes(M.shape[0])                        # Integrating the Ridge term into the final matrix
+
+        self.beta = np.linalg.solve(M, N)
+        return self
+
+
+    # Standard predict function.
+    def predict(self, X):
+        if self.fit_intercept:
+            X = np.concatenate([X, np.ones((X.shape[0], 1))], axis=1)
+        return np.matmul(X, self.beta)
+
+
+    '''
+    Predicts the sum of each block contained in Xs.
+    Used to check the prediction results against the sum values ys.
+    '''
+    def predict_block(self, Xs):
+        results = [0]*len(Xs)
+        for i in range(len(Xs)):
+            results[i] = np.sum(self.predict(Xs[i]).reshape(-1))
+        return results
+
+
+
+
+
+
+
+
+
+
+
+
